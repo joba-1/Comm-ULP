6813e948675976b2e7c34af4858e887f9ad58485
Joachim Banzhaf <Joachim.Banzhaf@gmail.com>
2019-02-19 18:59:25 +0100

    main cores go to sleep while no data from ulp

--- main/main.c
+++ main/main.c
@@ -2,13 +2,13 @@
 JoBa1 ESP32 ULP comm example
 
 Load and start an ULP programm, then read data stream from ULP
-Later: go to sleep if no new messages and get woken up by ULP if comm buffer fills up
+If there is no more data, go to sleep and wake up on ULP trigger
 */
 
 #include <stdio.h>         // printf(), putchar();
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h" // for task and timing
-// #include "esp_sleep.h"  // esp_sleep_enable_ulp_wakeup(), esp_deep_sleep_start()
+#include "esp_sleep.h"     // esp_sleep_enable_ulp_wakeup(), esp_deep_sleep_start()
 
 #include "ulp-util.h"      // my ulp_init(), ulp_start(), ulp_get() and .globals
 
@@ -38,14 +38,13 @@ void read_ulp() {
     vTaskDelay(1 / portTICK_PERIOD_MS); // wait until ulp is initialized
   buffer_end_ulp = buffer_start_ulp + (&ulp_buffer_end - &ulp_buffer);
 
-  for(;;) {
-    if( get(&item) ) {
-      putchar(item & 0xff);  // this example uses only the low byte of the value
-    }
-    else {
-      vTaskDelay(1 / portTICK_PERIOD_MS); // wait for ulp providing more data
-    }
+  while( get(&item) ) {
+    putchar(item & 0xff);  // this example uses only the low byte of the value
   }
+
+  printf("Going to sleep now.\n");
+  fflush(stdout);
+  esp_deep_sleep_start();
 }
 
 
@@ -53,21 +52,17 @@ void app_main()
 {
   printf("Hello Comm ULP!\n");
 
-  uint32_t wakeups[] = { 2*1000 };
-  ulp_init(wakeups, sizeof(wakeups)/sizeof(*wakeups));
-  ulp_start();
-
-  printf("Started ULP.\n");
-
-  // in this example the ULP simply increments a counter when it wakes up,
-  // writes the value as hex string into the ringbuffer and goes to sleep.
-  xTaskCreate(read_ulp, "read_ulp", 4*configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-
-  /*
-  printf("Going to sleep now.\n");
-  fflush(stdout);
+  if( esp_sleep_get_wakeup_cause() != ESP_SLEEP_WAKEUP_ULP ) {
+    uint32_t wakeups[] = { 200*1000 };
+    ulp_init(wakeups, sizeof(wakeups)/sizeof(*wakeups));
+    ulp_start();
+    printf("Started ULP.\n");
+  }
 
   ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup() );
-  esp_deep_sleep_start();
-  */
+
+  // in this example the ULP simply increments a counter when it wakes up,
+  // writes the value as hex string into the ringbuffer,
+  // wakes up the main core if the buffer is full, then goes to sleep.
+  xTaskCreate(read_ulp, "read_ulp", 4*configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
 }
